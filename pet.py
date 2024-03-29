fbe9913ce70c8e1a9969823277d49f316b5cc38e
Jorge André <jorge.matosandre@gmail.com>
2024-03-29 18:01:33 +0100

    Add initial pet class

---
+++ pet.py
@@ -0,0 +1,5 @@
+class Pet:
+    def __init__(self, pos=(0,0)):
+        self.pos = pos
+        self.happiness = 0
+        self.age = 0
--- pituxo.py
+++ pituxo.py
@@ -1,11 +1,13 @@
 # Example file showing a circle moving on screen
 import pygame
+import random
 
 # pygame setup
 pygame.init()
 
 size = ()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((410, 410), pygame.SCALED)
+pygame.display.set_caption('Pituxo')
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -23,17 +25,25 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "red", player_pos, 10)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    
+    mov_y = random.randint(-50,50) * dt
+    mov_x = random.randint(-50,50) * dt
+
+    if player_pos.y + mov_y in range (0, screen.get_height()):
+        player_pos.y += mov_y 
+    else:
+        player_pos.y -= mov_y
+
+    if player_pos.x + mov_x in range(0, screen.get_width()):
+        player_pos.x += mov_x
+    else:
+        player_pos.x -= mov_x
+
+    
+    
 
     # Render graphics
     # flip() the display to put your work on screen
